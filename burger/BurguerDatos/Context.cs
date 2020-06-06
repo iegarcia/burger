8cfa70b41ebb8f8b7e0405bc7d43a9e5385dab02
lucila.purcaro <lucila.purcaro@despegar.com>
2020-06-06 17:04:54 -0300

    db v1

---
+++ burger/BurguerDatos/Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using burger.Entidades;
+
+namespace burger.BurguerDatos
+{
+    public class Context : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
--- burger/Controllers/HomeController.cs
+++ burger/Controllers/HomeController.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using burger.BurguerDatos;
+using burger.Entidades;
 
 namespace burger.Controllers
 {
     public class HomeController : Controller
     {
+
+        //public List<Producto> Productos
+        //{
+        //    get
+        //    {
+        //        if (Session["Productos"] == null)
+        //            Session["Productos"] = new List<Producto>();
+        //        return (List<Producto>)Session["Productos"];
+        //    }
+        //}
+
+        //public List<Producto> ProductosCarrito
+        //{
+        //    get
+        //    {
+        //        if (Session["ProductosCarrito"] == null)
+        //            Session["ProductosCarrito"] = new List<Producto>();
+        //        return (List<Producto>)Session["ProductosCarrito"];
+        //    }
+        //}
+
+        
+
         public ActionResult Index()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public void AddProduct(Producto producto) {
+            Context contexto = new Context();
+            contexto.Productos.Add(producto);
+            contexto.SaveChanges();
+
+        }
+
+
     }
 }
