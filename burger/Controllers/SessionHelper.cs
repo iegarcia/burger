ce818c760593072f8e268c9303316f917201b2b3
iegarcia <nachogarcia003@gmail.com>
2020-06-17 18:25:42 -0300

    User Logged & other details

---
+++ burger/Controllers/SessionHelper.cs
@@ -0,0 +1,27 @@
+﻿using burger.Entidades;
+using burger.Models;
+using burger.Reglas;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace burger.Controllers
+{
+    public class SessionHelper
+    {
+        public static User UsuarioLogueado
+        {
+            get
+            {
+                if (HttpContext.Current.Session["UsuarioLogueado"] == null)
+                    return null;
+                return (User)HttpContext.Current.Session["UsuarioLogueado"];
+            }
+            set
+            {
+                HttpContext.Current.Session["UsuarioLogueado"] = value;
+            }
+        }
+    }
+}
