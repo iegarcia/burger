e728d751803b357fc5c970ec2444d7dc4c734b36
iegarcia <nachogarcia003@gmail.com>
2020-07-01 18:33:11 -0300

    Detalles

--- burger/Controllers/PedidoStatusController.cs
+++ burger/Controllers/PedidoStatusController.cs
@@ -1,6 +1,7 @@
 ﻿using burger.Entidades;
 using burger.Models;
 using burger.Reglas;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace burger.Controllers
@@ -11,12 +12,20 @@ namespace burger.Controllers
         public ActionResult SeguirEnvio(int id)
         {
             Pedido p = RNPedidos.BuscarPedido(id);
-            PedidoModel res = new PedidoModel
+            PedidoModel pEncontrado = new PedidoModel
             {
                 PedidoId = p.Id,
+                DatosConsumidor = new DeliveryModel
+                {
+                    Calle = p.Calle,
+                    Numero = p.Numero,
+                    Piso = p.Piso,
+                    Depto = p.Depto,
+                    Telefono = p.Telefono,
+                },
                 EstadoDelPedido = p.EstadoPedido
             };
-            return View("PedidoStatus", res);
+            return View("PedidoStatus", pEncontrado);
         }
 
     }
